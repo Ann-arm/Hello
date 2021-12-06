87cd6f60cff057ba9fc077c0768abf28d40c5a82
Ann <annarmash008@gmail.com>
2021-12-06 11:44:05 +0200

    Homework done

--- src/main/java/oop_practice/cat_dog/animal/Animal.java
+++ src/main/java/oop_practice/cat_dog/animal/Animal.java
@@ -3,6 +3,6 @@ package oop_practice.cat_dog.animal;
 
 public class Animal {
     public void sound() {
-        System.out.println("Animal sound");
+        System.out.println("Information about animals");
     }
 }
